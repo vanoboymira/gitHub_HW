91c5b006731d2e844345de16884ccb2f595cad47
Ivan <vanoboy@yandex.ru>
2023-01-11 15:32:19 +0300

    HM_done

---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,20 @@
+
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+
+
+public class SearchTest {
+
+
+    @Test
+    void commitTest(){
+        open("https://the-internet.herokuapp.com");
+
+
+    }
+}
